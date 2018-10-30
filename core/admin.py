7c94320e9e70e17a16c9f88d86324f122f810a53
giulia <giulia@alfa.simplyopen.org>
2018-10-30 12:17:07 +0100

    registered admin models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from .models import Libro, Autore, Genere, SottoGenere, Editore
 # Register your models here.
+
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Autore)
+class AutoreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Genere)
+class GenereAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SottoGenere)
+class SottoGenereAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Editore)
+class EditoreAdmin(admin.ModelAdmin):
+    pass
