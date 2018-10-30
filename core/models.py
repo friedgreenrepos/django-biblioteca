f9974d0c94a660f22305c1eb2b53080b0fb1b8a3
giulia <giulia@alfa.simplyopen.org>
2018-10-30 12:18:06 +0100

    fix initial models and migration

--- core/models.py
+++ core/models.py
@@ -7,31 +7,66 @@ class Genere(models.Model):
     nome = models.CharField(max_length=100)
 
     class Meta:
-        ordering = ('nome',)
+        verbose_name_plural = 'Generi'
 
     def __str__(self):
         return self.nome
 
 
-class InfoLibro(models.Model):
-    isbn = models.CharField('ISBN', max_length=13, unique=True)
-    titolo = models.CharField(max_length=100)
-    autori = models.ManyToManyField('Autore')
-    descrizione = models.TextField(blank=True)
-    editore = models.CharField(max_length=100)
-    genere = models.ManyToManyField('Genere')
-
+class SottoGenere(models.Model):
+    nome = models.CharField(max_length=100)
+    padre = models.ForeignKey(Genere, on_delete=models.CASCADE)
 
     class Meta:
-        ordering = ('titolo',)
+        verbose_name_plural = 'Sottogeneri'
 
     def __str__(self):
-        return self.titolo
+        return self.nome
 
 
-class TrackLibro(models.Model):
-    pass
+class Editore(models.Model):
+    nome = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = 'Editori'
+
+    def __str__(self):
+        return self.nome
 
 
 class Autore(models.Model):
-    pass
+    nome = models.CharField(max_length=50)
+    cognome = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = 'Autori'
+
+    def __str__(self):
+        return '{} {}'.format(self.cognome, self.nome)
+
+
+class TrackLibro(models.Model):
+    disponibile = models.BooleanField(default=True)
+    data_restituzione = models.DateField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Libro(TrackLibro):
+    isbn = models.CharField(max_length=13, unique=True, verbose_name='ISBN')
+    titolo = models.CharField(max_length=100)
+    autori = models.ManyToManyField('Autore')
+    descrizione = models.TextField(blank=True)
+    editore = models.ForeignKey(Editore, on_delete=models.PROTECT)
+    genere = models.ManyToManyField(Genere)
+    sottogenere = models.ManyToManyField(SottoGenere)
+    collana = models.CharField(max_length=100)
+
+
+    class Meta:
+        verbose_name_plural = 'Libri'
+        ordering = ['titolo']
+
+    def __str__(self):
+        return self.titolo
